1e0ac1ee8c4b7aa6c94f4c3b618dfe6f929d41fe
eburnaman <eburnaman@gmail.com>
2019-08-25 16:05:12 -0400

    revamping javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,45 +1,25 @@
-//Objective 1: Setting the 8 minute timer & alerts
+// //mercy
+// class DoggyData {
+//     constructor (quests, answs, corrs, image=null) {
+//         this.question = quests;
+//         this.answer = answs;
+//         this.correct = corrs;
+//         this.image = image;
+//     }
+// }
+// // layout of questions using constructor form
+// const DogData = [
+//     new DoggyData ("What breed of dog is snoopy?", ["Beagle", "Hound", "Border Collie", "Boston Terrier"], 0 ),
+//     new DoggyData ("What's the WORLDS most popular dog breed?", ["Golden Retriever", "German Shepherds", "Rottweiler", "Labrador"], 3 ),
+//     new DoggyData ("What breed of dog is the favourite of Queen Elizabeth?", ["King Charles Cavalier Spaniel", "Pug", "Corgi", "ChowChow"], 2 ),
+//     new DoggyData ("Where are a dog's sweat glands located?", ["Nose", "Tail", "Mouth", "Paws"], 3 ),
+//     new DoggyData ("What breed has a black tongue?", ["Chow Chow", "Chihuahua", "Bloodhound", "Rat Terrier"], 0 ),
+//     new DoggyData ("Which human organ do dogs lack?", ["Prostate", "Pancreas", "Appendix", "Small Intestine"], 2 ),
+//     new DoggyData ("Why do dogs curl up when they sleep?", ["To make themselves invisibile", "To be warm and comfy", "To protect their organs", "To show submission"], 2 ),
+//     new DoggyData ("What breed has six toes on each paw?", ["Husky", "Saint Bernard", "Norweigian Lundehund", "Cairn Terrier"], 0 ),
+//     new DoggyData ("How many muscles do dogs have in their ears?", ["~3", "~8", "Only 1", "18 or more"], 0 ),
+//     new DoggyData ("Which four colors can a dog see", ["Black, white, blue, yellow", "Gray, gray, gray, gray", "Black, green, red, blue", "Yellow, purple, orange, red"], 0 ),
+//     new DoggyData ("Which sense are puppies born without?", ["Sight", "Taste", "Hearing", "Smell"], 2 ),
+//     new DoggyData ("How many taste buds do dogs have?", ["400", "1,700", "900", "100"], 1 ),
+// ];
 
-var TriviaTimer = 0
-var CountDown = setInterval(function() {
-    var minutes = Math.Floor ((distance % (1000 * 60 * 60)) / 1000 * 60));
-    var seconds = Math.Floor ((distance % (1000 * 60)) / 1000));
-
-    document.getElementById("timer").innerHTML = 
-}
-
-//Objective 2: defining the Array of Questions! Sally taught me how to use the constructor so kudos & credit to her!
-class DoggyData {
-    construction (quests, answs, corrs, image=null) {
-        this.question = quests;
-        this.answer = answs;
-        this.correct = corrs;
-        this.image = image;
-    }
-}
-// layout of questions using constructor form
-const DogData = [
-    new DoggyData ("What breed of dog is snoopy?", ["Beagle", "Hound", "Border Collie", "Boston Terrier"], 0 ),
-    new DoggyData ("What breed of dog is snoopy?", ["Beagle", "Hound", "Border Collie", "Boston Terrier"], 0 ),
-    new DoggyData ("What breed of dog is snoopy?", ["Beagle", "Hound", "Border Collie", "Boston Terrier"], 0 ),
-    new DoggyData ("What breed of dog is snoopy?", ["Beagle", "Hound", "Border Collie", "Boston Terrier"], 0 ),
-    new DoggyData ("What breed of dog is snoopy?", ["Beagle", "Hound", "Border Collie", "Boston Terrier"], 0 ),
-    new DoggyData ("What breed of dog is snoopy?", ["Beagle", "Hound", "Border Collie", "Boston Terrier"], 0 ),
-    new DoggyData ("What breed of dog is snoopy?", ["Beagle", "Hound", "Border Collie", "Boston Terrier"], 0 ),
-    new DoggyData ("What breed of dog is snoopy?", ["Beagle", "Hound", "Border Collie", "Boston Terrier"], 0 ),
-    new DoggyData ("What breed of dog is snoopy?", ["Beagle", "Hound", "Border Collie", "Boston Terrier"], 0 ),
-    new DoggyData ("What breed of dog is snoopy?", ["Beagle", "Hound", "Border Collie", "Boston Terrier"], 0 ),
-    new DoggyData ("What breed of dog is snoopy?", ["Beagle", "Hound", "Border Collie", "Boston Terrier"], 0 ),
-    new DoggyData ("What breed of dog is snoopy?", ["Beagle", "Hound", "Border Collie", "Boston Terrier"], 0 ),
-];
-
-//Variables
-var currentAnswer = 0;
-var questionsAsked = 0;
-var numCorrect = 0;
-var numWrong = 0;
-
-//functions
-$(#startTrivia).on("click", function() {
-    $("#startGame").replaceWith()
-} )

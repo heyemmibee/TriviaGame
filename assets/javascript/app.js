d263b5af8f0d6cca88d760ebf0385bd83bc7fdcd
eburnaman <eburnaman@gmail.com>
2019-09-06 22:10:57 -0400

    in progress

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 // //mercy
 // class DoggyData {
-//     constructor (quests, answs, corrs, image=null) {
+//     constructor (quests, answs, corrs, image) {
 //         this.question = quests;
 //         this.answer = answs;
 //         this.correct = corrs;
@@ -9,7 +9,7 @@
 // }
 // // layout of questions using constructor form
 // const DogData = [
-//     new DoggyData ("What breed of dog is snoopy?", ["Beagle", "Hound", "Border Collie", "Boston Terrier"], 0 ),
+//     new DoggyData ("What breed of dog is Snoopy (from Charlie Brown)?", ["Beagle", "Hound", "Border Collie", "Boston Terrier"], 0 ),
 //     new DoggyData ("What's the WORLDS most popular dog breed?", ["Golden Retriever", "German Shepherds", "Rottweiler", "Labrador"], 3 ),
 //     new DoggyData ("What breed of dog is the favourite of Queen Elizabeth?", ["King Charles Cavalier Spaniel", "Pug", "Corgi", "ChowChow"], 2 ),
 //     new DoggyData ("Where are a dog's sweat glands located?", ["Nose", "Tail", "Mouth", "Paws"], 3 ),
@@ -23,3 +23,50 @@
 //     new DoggyData ("How many taste buds do dogs have?", ["400", "1,700", "900", "100"], 1 ),
 // ];
 
+var area = $('#quiz-space');
+
+var trivia = { 
+    correct: 0,
+    incorrect: 0,
+
+    start: function() {
+        $('#quiz-space').prepend("<h2>Good Luck!</h2>");
+        $("#start-trivia").remove();
+        
+        for (var i = 0; i < doggydata.length; i++) {
+        area.append('<h2>' + doggydata[i].question + '</h2');
+        for (var a = 0; a < doggydata[i].answer.length; a++) {
+        area.append("<input type='radio' name='question: " + i + 
+        "'value: '" + doggydata[i].answer[j] + "''>" + doggydata[i].answer[j]);    
+        }
+    }
+    card.append("<button id='done'>Done</button>");
+},
+    done: function() {
+        var selection = area.children("input:checked");
+        for (var i = 0; i < selection.length; i++) {
+            if ($(selection[i]).val() === doggydata[i].answer) {
+              trivia.correct++;
+            } else {
+                trivia.incorrect++;
+            }  
+            }
+            this.result();
+        },
+    result: function() {
+        area.html('<h2>You did it!</h2>');
+        area.append('<h3>Correct Answers: ' + this.correct + '</h3>');
+        area.append('<h3>Incorrect Answers: ' + this.incorrect + '</h3>');
+    }
+    };
+
+//click events
+
+$(document).on("click", "#start", function() {
+    game.start();
+  });
+  
+  $(document).on("click", "#done", function() {
+    game.done();
+  });
+  

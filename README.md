36f9271c782a74785bde7e37f40a251e15110074
eburnaman <eburnaman@gmail.com>
2019-09-08 13:40:22 -0400

    basic javascript working but correct asnwers not functioning

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# TriviaGame
+# Trivia Game
+
+Trivia Game 'Doggy Trivia' uses basic Javascript, jQuery, HTML and Boostrap CSS. Doggy Trivia gives you a set time 90 seconds to answer 12 questions about your canine friends. By clicking start the user begins the trivia game, populating the questions and starting the time. These are stopped upon clicking done.
+
+## Getting Started
+
+This application is able to be run locally via deployment link below:
+
+
+## Prerequisites
+
+Not needed
+
+## Built With
+
+* [JavaScript](https://www.javascript.com/) - Programming language.
+* [jQuery](https://jquery.com/) - Javascript library.
+* [Bootstrap](https://getbootstrap.com/docs/3.3/) - Front-end HTML, CSS, javascript framework for creating responsive web applications. 
+
+## Contributing
+
+Please feel free to offer any suggestions or improvements.
+
+## Author
+
+* **Emily Burnaman** - [Github](https://github.com/heyemmibee)
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,30 @@
-// //mercy
-// class DoggyData {
-//     constructor (quests, answs, corrs, image) {
-//         this.question = quests;
-//         this.answer = answs;
-//         this.correct = corrs;
-//         this.image = image;
-//     }
-// }
-// // layout of questions using constructor form
-// const DogData = [
-//     new DoggyData ("What breed of dog is Snoopy (from Charlie Brown)?", ["Beagle", "Hound", "Border Collie", "Boston Terrier"], 0 ),
-//     new DoggyData ("What's the WORLDS most popular dog breed?", ["Golden Retriever", "German Shepherds", "Rottweiler", "Labrador"], 3 ),
-//     new DoggyData ("What breed of dog is the favourite of Queen Elizabeth?", ["King Charles Cavalier Spaniel", "Pug", "Corgi", "ChowChow"], 2 ),
-//     new DoggyData ("Where are a dog's sweat glands located?", ["Nose", "Tail", "Mouth", "Paws"], 3 ),
-//     new DoggyData ("What breed has a black tongue?", ["Chow Chow", "Chihuahua", "Bloodhound", "Rat Terrier"], 0 ),
-//     new DoggyData ("Which human organ do dogs lack?", ["Prostate", "Pancreas", "Appendix", "Small Intestine"], 2 ),
-//     new DoggyData ("Why do dogs curl up when they sleep?", ["To make themselves invisibile", "To be warm and comfy", "To protect their organs", "To show submission"], 2 ),
-//     new DoggyData ("What breed has six toes on each paw?", ["Husky", "Saint Bernard", "Norweigian Lundehund", "Cairn Terrier"], 0 ),
-//     new DoggyData ("How many muscles do dogs have in their ears?", ["~3", "~8", "Only 1", "18 or more"], 0 ),
-//     new DoggyData ("Which four colors can a dog see", ["Black, white, blue, yellow", "Gray, gray, gray, gray", "Black, green, red, blue", "Yellow, purple, orange, red"], 0 ),
-//     new DoggyData ("Which sense are puppies born without?", ["Sight", "Taste", "Hearing", "Smell"], 2 ),
-//     new DoggyData ("How many taste buds do dogs have?", ["400", "1,700", "900", "100"], 1 ),
-// ];
+//object constructor for dog trivia questions
 
+function DoggyData(question, answer, rightanswer) {
+    this.question = question;
+    this.answer = answer;
+    this.rightanswer = rightanswer;
+}
+
+var doggydatas = [
+    new DoggyData ("What breed of dog is Snoopy (from Charlie Brown)?", ["Beagle", "Hound", "Border Collie", "Boston Terrier"], 0 ),
+    new DoggyData ("What's the WORLDS most popular dog breed?", ["Golden Retriever", "German Shepherds", "Rottweiler", "Labrador"], 3 ),
+    new DoggyData ("What breed of dog is the favourite of Queen Elizabeth?", ["King Charles Cavalier Spaniel", "Pug", "Corgi", "ChowChow"], 2 ),
+    new DoggyData ("Where are a dog's sweat glands located?", ["Nose", "Tail", "Mouth", "Paws"], 3 ),
+    new DoggyData ("What breed has a black tongue?", ["Chow Chow", "Chihuahua", "Bloodhound", "Rat Terrier"], 0 ),
+    new DoggyData ("Which human organ do dogs lack?", ["Prostate", "Pancreas", "Appendix", "Small Intestine"], 2 ),
+    new DoggyData ("Why do dogs curl up when they sleep?", ["To make themselves invisibile", "To be warm and comfy", "To protect their organs", "To show submission"], 2 ),
+    new DoggyData ("What breed has six toes on each paw?", ["Husky", "Saint Bernard", "Norweigian Lundehund", "Cairn Terrier"], 0 ),
+    new DoggyData ("How many muscles do dogs have in their ears?", ["~3", "~8", "Only 1", "18 or more"], 0 ),
+    new DoggyData ("Which four colors can a dog see", ["Black, white, blue, yellow", "Gray, gray, gray, gray", "Black, green, red, blue", "Yellow, purple, orange, red"], 0 ),
+    new DoggyData ("Which sense are puppies born without?", ["Sight", "Taste", "Hearing", "Smell"], 2 ),
+    new DoggyData ("How many taste buds do dogs have?", ["400", "1,700", "900", "100"], 1 ),
+];
+
+//testing arrays works
+console.log(doggydatas);
+
+//pup-ulate trivia questions, answers and number answered correctly/incorrectly
 var area = $('#quiz-space');
 
 var trivia = { 
@@ -30,31 +32,34 @@ var trivia = {
     incorrect: 0,
 
     start: function() {
-        $('#quiz-space').prepend("<h2>Good Luck!</h2>");
+        $('#quiz-space').prepend(
+            "<h2>Don't labradoodle around, let's get started! </h2>");
         $("#start-trivia").remove();
         
-        for (var i = 0; i < doggydata.length; i++) {
-        area.append('<h2>' + doggydata[i].question + '</h2');
-        for (var a = 0; a < doggydata[i].answer.length; a++) {
-        area.append("<input type='radio' name='question: " + i + 
-        "'value: '" + doggydata[i].answer[j] + "''>" + doggydata[i].answer[j]);    
+        for (var d = 0; d < doggydatas.length; d++) {
+        area.append('<h2>' + doggydatas[d].question + '</h2');
+        for (var a = 0; a < doggydatas[d].answer.length; a++) {
+        area.append("<input type='radio' name='question: " + d + 
+        "'value: '" + doggydatas[d].answer[a] + "''>" + doggydatas[d].answer[a]);    
         }
     }
-    card.append("<button id='done'>Done</button>");
+
+    area.append("<br><br><button id='done' class='btn btn-danger'>Done</button>");
 },
     done: function() {
         var selection = area.children("input:checked");
-        for (var i = 0; i < selection.length; i++) {
-            if ($(selection[i]).val() === doggydata[i].answer) {
+        for (var s = 0; s < selection.length; s++) {
+            if ($(selection[s]).val() === doggydatas[s].rightanswer) {
               trivia.correct++;
             } else {
                 trivia.incorrect++;
             }  
-            }
-            this.result();
+        }
+        this.result();
         },
+        
     result: function() {
-        area.html('<h2>You did it!</h2>');
+        area.html("<h2>Don't Stop Retrieving</h2>");
         area.append('<h3>Correct Answers: ' + this.correct + '</h3>');
         area.append('<h3>Incorrect Answers: ' + this.incorrect + '</h3>');
     }
@@ -62,11 +67,11 @@ var trivia = {
 
 //click events
 
-$(document).on("click", "#start", function() {
-    game.start();
+$(document).on("click", "#start-trivia", function() {
+    trivia.start();
   });
   
   $(document).on("click", "#done", function() {
-    game.done();
+    trivia.done();
   });
   
